4c3d271634f3fed3cbe55f613d31f40938ec2c24
Javakin <danielharaldson@yahoo.com>
2016-03-26 00:24:05 +0100

    processen er skrevet og to signals er added, men systemet er ikke testet. skal steppe ned med en faktor 5

--- Projeckt/Komponenter/StepDown1KHz/StepDown1KHz.vhd
+++ Projeckt/Komponenter/StepDown1KHz/StepDown1KHz.vhd
@@ -19,7 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -34,18 +34,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 ----------------------------------------------------------------------------------
 entity StepDown1KHz is
     Port ( CLK_50MHz : in  STD_LOGIC;
-           CLK_1KHZ : out  STD_LOGIC);
+           CLK_1KHz : out  STD_LOGIC);
 end StepDown1KHz;
 
 
 architecture Behavioral of StepDown1KHz is
 -- define internal signals
 ----------------------------------------------------------------------------------
-
+signal prescaler : STD_LOGIC_VECTOR (25 downto 0) := (others => '0');
+signal output	  : STD_LOGIC	:= '0';
 
 begin
 -- Process stepdown 1/50.000
 ----------------------------------------------------------------------------------
+StepDown1K :
+Process (CLK_50MHz)
+	begin
+		if rising_edge(CLK_50MHz) then
+			if prescaler < "101" then
+				prescaler 	<= prescaler + 1;
+			else
+				prescaler 	<= (others => '0');
+				output 		<= not output;	
+			end if;
+		end if;
+	end process;
+
+CLK_1kHz <= output;
+
 
 end Behavioral;
 
