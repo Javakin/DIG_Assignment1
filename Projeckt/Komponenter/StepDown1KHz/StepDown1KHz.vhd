7fafdc3a4d1efc24f058ff8f6d8c90700b0fa5fb
Javakin <danielharaldson@yahoo.com>
2016-03-26 00:13:09 +0100

    added comments

--- Projeckt/Komponenter/StepDown1KHz/StepDown1KHz.vhd
+++ Projeckt/Komponenter/StepDown1KHz/StepDown1KHz.vhd
@@ -29,15 +29,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+
+-- Define ports
+----------------------------------------------------------------------------------
 entity StepDown1KHz is
     Port ( CLK_50MHz : in  STD_LOGIC;
            CLK_1KHZ : out  STD_LOGIC);
 end StepDown1KHz;
 
+
 architecture Behavioral of StepDown1KHz is
+-- define internal signals
+----------------------------------------------------------------------------------
+
 
 begin
-
+-- Process stepdown 1/50.000
+----------------------------------------------------------------------------------
 
 end Behavioral;
+
+
+
+
+
+
+
 
