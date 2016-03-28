0ab6fc8b5868d3e74f4da522c9c108c3634628e5
Javakin <danielharaldson@yahoo.com>
2016-03-28 14:48:40 +0200

    noget virker, men mangler at teste resten

---
+++ Projeckt/Komponenter/Lach_Time_Lap/TEST_Lach.vhd
@@ -0,0 +1,111 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:34:12 03/28/2016
+-- Design Name:   
+-- Module Name:   D:/Git/DIG_Assignment1/Projeckt/Komponenter/Lach_Time_Lap/TEST_Lach.vhd
+-- Project Name:  Lach_Time_Lap
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Time_Lap_Lach
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.STD_LOGIC_unsigned.all;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY TEST_Lach IS
+END TEST_Lach;
+ 
+ARCHITECTURE behavior OF TEST_Lach IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Time_Lap_Lach
+    PORT(
+         LAP 			: IN  std_logic;
+         IN_TEENTH 	: IN  std_logic_vector(3 downto 0);
+         IN_SEC 		: IN  std_logic_vector(7 downto 0);
+         IN_MIN 		: IN  std_logic_vector(7 downto 0);
+         OUT_TEENTH 	: OUT  std_logic_vector(3 downto 0);
+         OUT_SEC 		: OUT  std_logic_vector(7 downto 0);
+         OUT_MIN 		: OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal LAP 			: std_logic := '0';
+   signal IN_TEENTH 	: std_logic_vector(3 downto 0) := "0001";
+   signal IN_SEC 		: std_logic_vector(7 downto 0) := "00000010";
+   signal IN_MIN 		: std_logic_vector(7 downto 0) := "00000011";
+
+ 	--Outputs
+   signal OUT_TEENTH : std_logic_vector(3 downto 0);
+   signal OUT_SEC 	: std_logic_vector(7 downto 0);
+   signal OUT_MIN 	: std_logic_vector(7 downto 0);
+ 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Time_Lap_Lach PORT MAP (
+          LAP 			=> LAP,
+          IN_TEENTH 	=> IN_TEENTH,
+          IN_SEC 		=> IN_SEC,
+          IN_MIN 		=> IN_MIN,
+          OUT_TEENTH => OUT_TEENTH,
+          OUT_SEC 	=> OUT_SEC,
+          OUT_MIN 	=> OUT_MIN
+        );
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+
+      wait for 100 us;	
+		LAP <= '1';
+     
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+	
+	add_one: process
+   begin		
+      -- hold reset state for 100 ns.
+	
+
+      -- insert stimulus here 
+		IN_TEENTH 	<= IN_TEENTH + 1;
+		IN_SEC 		<= IN_SEC + 1;
+		IN_MIN 		<= IN_MIN + 1;
+
+      wait for 10 ns;
+
+      wait;
+   end process;
+
+END;
---
+++ Projeckt/Komponenter/Lach_Time_Lap/isim/TEST_Lach_isim_beh.exe.sim/work/a_3391537542_3212880686.c
@@ -0,0 +1,263 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "D:/Git/DIG_Assignment1/Projeckt/Komponenter/Lach_Time_Lap/Time_Lap_Lach.vhd";
+extern char *IEEE_P_2592010699;
+
+unsigned char ieee_p_2592010699_sub_1744673427_503743352(char *, char *, unsigned int , unsigned int );
+
+
+static void work_a_3391537542_3212880686_p_0(char *t0)
+{
+    char *t1;
+    unsigned char t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(60, ng0);
+    t1 = (t0 + 992U);
+    t2 = ieee_p_2592010699_sub_1744673427_503743352(IEEE_P_2592010699, t1, 0U, 0U);
+    if (t2 != 0)
+        goto LAB2;
+
+LAB4:
+LAB3:    t1 = (t0 + 4696);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(62, ng0);
+    t3 = (t0 + 1192U);
+    t4 = *((char **)t3);
+    t3 = (t0 + 4824);
+    t5 = (t3 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t4, 4U);
+    xsi_driver_first_trans_fast(t3);
+    xsi_set_current_line(63, ng0);
+    t1 = (t0 + 1352U);
+    t3 = *((char **)t1);
+    t1 = (t0 + 4888);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t3, 8U);
+    xsi_driver_first_trans_fast(t1);
+    xsi_set_current_line(64, ng0);
+    t1 = (t0 + 1512U);
+    t3 = *((char **)t1);
+    t1 = (t0 + 4952);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t3, 8U);
+    xsi_driver_first_trans_fast(t1);
+    goto LAB3;
+
+}
+
+static void work_a_3391537542_3212880686_p_1(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    char *t12;
+    char *t13;
+    char *t14;
+    char *t15;
+    char *t16;
+
+LAB0:    xsi_set_current_line(69, ng0);
+    t1 = (t0 + 1032U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB3;
+
+LAB4:
+LAB5:    t10 = (t0 + 1192U);
+    t11 = *((char **)t10);
+    t10 = (t0 + 5016);
+    t12 = (t10 + 56U);
+    t13 = *((char **)t12);
+    t14 = (t13 + 56U);
+    t15 = *((char **)t14);
+    memcpy(t15, t11, 4U);
+    xsi_driver_first_trans_fast_port(t10);
+
+LAB2:    t16 = (t0 + 4712);
+    *((int *)t16) = 1;
+
+LAB1:    return;
+LAB3:    t1 = (t0 + 2152U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 5016);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 4U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB2;
+
+LAB6:    goto LAB2;
+
+}
+
+static void work_a_3391537542_3212880686_p_2(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    char *t12;
+    char *t13;
+    char *t14;
+    char *t15;
+    char *t16;
+
+LAB0:    xsi_set_current_line(70, ng0);
+    t1 = (t0 + 1032U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB3;
+
+LAB4:
+LAB5:    t10 = (t0 + 1352U);
+    t11 = *((char **)t10);
+    t10 = (t0 + 5080);
+    t12 = (t10 + 56U);
+    t13 = *((char **)t12);
+    t14 = (t13 + 56U);
+    t15 = *((char **)t14);
+    memcpy(t15, t11, 8U);
+    xsi_driver_first_trans_fast_port(t10);
+
+LAB2:    t16 = (t0 + 4728);
+    *((int *)t16) = 1;
+
+LAB1:    return;
+LAB3:    t1 = (t0 + 2312U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 5080);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 8U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB2;
+
+LAB6:    goto LAB2;
+
+}
+
+static void work_a_3391537542_3212880686_p_3(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    char *t12;
+    char *t13;
+    char *t14;
+    char *t15;
+    char *t16;
+
+LAB0:    xsi_set_current_line(71, ng0);
+    t1 = (t0 + 1032U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)3);
+    if (t4 != 0)
+        goto LAB3;
+
+LAB4:
+LAB5:    t10 = (t0 + 1512U);
+    t11 = *((char **)t10);
+    t10 = (t0 + 5144);
+    t12 = (t10 + 56U);
+    t13 = *((char **)t12);
+    t14 = (t13 + 56U);
+    t15 = *((char **)t14);
+    memcpy(t15, t11, 8U);
+    xsi_driver_first_trans_fast_port(t10);
+
+LAB2:    t16 = (t0 + 4744);
+    *((int *)t16) = 1;
+
+LAB1:    return;
+LAB3:    t1 = (t0 + 2472U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 5144);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 8U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB2;
+
+LAB6:    goto LAB2;
+
+}
+
+
+extern void work_a_3391537542_3212880686_init()
+{
+	static char *pe[] = {(void *)work_a_3391537542_3212880686_p_0,(void *)work_a_3391537542_3212880686_p_1,(void *)work_a_3391537542_3212880686_p_2,(void *)work_a_3391537542_3212880686_p_3};
+	xsi_register_didat("work_a_3391537542_3212880686", "isim/TEST_Lach_isim_beh.exe.sim/work/a_3391537542_3212880686.didat");
+	xsi_register_executes(pe);
+}
