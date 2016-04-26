cd391f3e4056eafc4b556be0afedd0126222173d
Javakin <danielharaldson@yahoo.com>
2016-04-26 17:16:41 +0200

    pass

--- Projeckt/Komponenter/ToggleButton/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "D:/Git/DIG_Assignment1/Projeckt/Komponenter/ToggleButton/TEST_ToggleButton_isim_beh.exe" -prj "D:/Git/DIG_Assignment1/Projeckt/Komponenter/ToggleButton/TEST_ToggleButton_beh.prj" "TEST_ToggleButton" 
