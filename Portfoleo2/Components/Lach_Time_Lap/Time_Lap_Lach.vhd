15f092594f80002bd1a58c14bce5b8ebae079fc1
Javakin <danielharaldson@yahoo.com>
2016-04-28 12:25:39 +0200

    pass

--- Portfoleo2/Components/Lach_Time_Lap/Time_Lap_Lach.vhd
+++ Portfoleo2/Components/Lach_Time_Lap/Time_Lap_Lach.vhd
@@ -34,41 +34,79 @@ use IEEE.STD_LOGIC_1164.ALL;
 ----------------------------------------------------------------------------------
 entity Time_Lap_Lach is
     Port ( LAP 			: in  STD_LOGIC;
-           IN_TEENTH 	: in  STD_LOGIC_VECTOR (3 downto 0);
-           IN_SEC 		: in  STD_LOGIC_VECTOR (7 downto 0);
-           IN_MIN 		: in  STD_LOGIC_VECTOR (7 downto 0);
-           OUT_TEENTH 	: out  STD_LOGIC_VECTOR (3 downto 0);
-           OUT_SEC 		: out  STD_LOGIC_VECTOR (7 downto 0);
-           OUT_MIN 		: out  STD_LOGIC_VECTOR (7 downto 0)
+			  CLEAR			: in 	STD_LOGIC;
+			  HIGH_SCORE	: in  STD_LOGIC;
+			  
+           IN_BCD1 		: in  STD_LOGIC_VECTOR (3 downto 0);
+           IN_BCD2 		: in  STD_LOGIC_VECTOR (3 downto 0);
+           IN_BCD3 		: in  STD_LOGIC_VECTOR (3 downto 0);
+			  IN_BCD4 		: in  STD_LOGIC_VECTOR (3 downto 0);
+			  
+           OUT_BCD1 	: out  STD_LOGIC_VECTOR (3 downto 0);
+           OUT_BCD2	: out  STD_LOGIC_VECTOR (3 downto 0);
+           OUT_BCD3	: out  STD_LOGIC_VECTOR (3 downto 0);
+			  OUT_BCD4	: out  STD_LOGIC_VECTOR (3 downto 0)
 			 );
 end Time_Lap_Lach;
 
 architecture Behavioral of Time_Lap_Lach is
 -- Define lach signals
 ----------------------------------------------------------------------------------
-signal  teenth	: STD_LOGIC_VECTOR ( 3 downto 0):= (others => '0');
-signal  sec	 	: STD_LOGIC_VECTOR ( 7 downto 0):= (others => '0');
-signal  min 	: STD_LOGIC_VECTOR ( 7 downto 0):= (others => '0');
+signal  bcd1	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '0');
+signal  bcd2 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '0');
+signal  bcd3 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '0');
+signal  bcd4 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '0');
 
-
+signal  hs1	 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '1');
+signal  hs2 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '1');
+signal  hs3 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '1');
+signal  hs4 	: STD_LOGIC_VECTOR ( 3 downto 0 ):= (others => '1');
 
 begin
-
+-- Latch functionality
+----------------------------------------------------------------------------------
 lach_process	: 
-PROCESS(LAP, IN_TEENTH, IN_SEC, IN_MIN)
+PROCESS(LAP, IN_BCD1, IN_BCD2, IN_BCD3, IN_BCD4)
 	BEGIN
 		if rising_edge(LAP) then
 			-- safe time in lach
-			teenth 	<= IN_TEENTH;
-			sec 		<= IN_SEC;
-			min 		<= IN_MIN;
+			bcd2		<= IN_BCD2;
+			bcd3		<= IN_BCD3;
+			bcd1  	<= IN_BCD1;
+			bcd4		<= IN_BCD4;
+			
+		end if;	
+	END PROCESS;
+	
+-- update high score
+----------------------------------------------------------------------------------
+update_highscore	: 
+PROCESS(LAP )
+	BEGIN
+		if rising_edge(LAP) then
+			-- safe time in lach
+			
 			
 		end if;	
 	END PROCESS;
 
-OUT_TEENTH 	<= teenth 	when LAP = '1' else IN_TEENTH;
-OUT_SEC 		<= sec 		when LAP = '1' else IN_SEC;
-OUT_MIN 		<= min 		when LAP = '1' else IN_MIN;
+
+OUT_BCD1 	<= IN_BCD1  when LAP = '0' else 
+					hs1		when LAP = '1' and HIGH_SCORE = '1' else
+					bcd1  	when LAP = '1' and HIGH_SCORE = '0';
+					
+OUT_BCD2		<= IN_BCD2  when LAP = '0' else 
+					hs2		when LAP = '1' and HIGH_SCORE = '1' else
+					bcd2  	when LAP = '1' and HIGH_SCORE = '0';
+
+OUT_BCD3 	<= IN_BCD3  when LAP = '0' else 
+					hs3 		when LAP = '1' and HIGH_SCORE = '1' else
+					bcd1  	when LAP = '1' and HIGH_SCORE = '0';
+					
+OUT_BCD4 	<= IN_BCD4  when LAP = '0' else 
+					hs4		when LAP = '1' and HIGH_SCORE = '1' else
+					bcd4  	when LAP = '1' and HIGH_SCORE = '0';
+
 
 end Behavioral;
 
