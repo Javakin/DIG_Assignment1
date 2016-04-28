0e4e6ea8993eb208d89921c71ef5e9ed45f0a8dd
Javakin <danielharaldson@yahoo.com>
2016-04-28 14:15:50 +0200

    don med latch

--- Portfoleo2/Components/Cnt999/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "D:/Git/DIG_Portfoleo2/Portfoleo2/Components/Cnt999/TEST_Cnt999_isim_beh.exe" -prj "D:/Git/DIG_Portfoleo2/Portfoleo2/Components/Cnt999/TEST_Cnt999_beh.prj" "TEST_Cnt999" 
